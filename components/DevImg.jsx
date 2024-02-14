3056c15115cfae2f7e8045307a31495d4b00c177
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2024-02-14 09:04:10 +0700

    make a about page

--- components/DevImg.jsx
+++ components/DevImg.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import Image from "next/image"
 
-const DevImg = () => {
-  return (
-    <div>DevImg</div>
-  )
+
+const DevImg = ({containerStyles, imgSrc}) => {
+  return <div className={`${containerStyles}`}>
+    <Image src={imgSrc} fill priority alt="" />
+  </div>
 }
 
 export default DevImg
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,6 +7,9 @@ import {
 } from "react-icons/ri";
 import { Button } from "./ui/button";
 import { Download, Send } from "lucide-react";
+import Socials from "./Socials";
+import DevImg from "./DevImg";
+import Badge from "./Badge";
 
 const Hero = () => {
   return (
@@ -34,9 +37,42 @@ const Hero = () => {
                 Download CV <Download size={18} />
               </Button>
             </div>
+            {/** socials */}
+            <Socials
+              containerStyles="flex gap-x-6 mx-auto xl:mx-0"
+              iconsStyles="text-foreground text-[22px] hover:text-primary 
+            transition-all"
+            />
           </div>
           {/*image*/}
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            {/** badge */}
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText="Years Of Experience"
+            />
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText="k"
+              badgeText="Finished Projects"
+            />
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText="k"
+              badgeText="Happy Clients"
+            />
+            <div className="bg-hero_shape2_light dark:bg-hero_shape_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
 
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
